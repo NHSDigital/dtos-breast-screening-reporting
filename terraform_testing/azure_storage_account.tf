7a21ad9d67c20616e140ea89556b698ebcf200bd
samw-nhs <172405776+samw-nhs@users.noreply.github.com>
2026-03-26 17:01:11 +0000

    DSTA-506: add terraform provisioning storage account and container, update gitignore

---
+++ terraform_testing/azure_storage_account.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group  = "breast-screening-reporting"
+  location        = "uksouth"
+  storage_account = "bsrtestdatalaketerraform"  # must be globally unique, lowercase, no hyphens
+  container       = "raw"
+}
+
+resource "azurerm_storage_account" "datalake" {
+  name                     = local.storage_account
+  resource_group_name      = local.resource_group
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  is_hns_enabled           = true
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "raw" {
+  name               = local.container
+  storage_account_id = azurerm_storage_account.datalake.id
+}
