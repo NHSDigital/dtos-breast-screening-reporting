65709f53f0ac896435badbe0ecda4d91777baa1e
samw-nhs <172405776+samw-nhs@users.noreply.github.com>
2026-04-01 16:42:09 +0100

    DSTA-493: update location

--- infrastructure/environments/dev/databricks_workspace.tf
+++ infrastructure/environments/dev/databricks_workspace.tf
@@ -1,6 +1,6 @@
 resource "azurerm_databricks_workspace" "main" {
   name                = "test-databricks-env-2"
   resource_group_name = local.resource_group
-  location            = "northeurope"
+  location            = data.azurerm_resource_group.main.location
   sku                 = "premium"
 }
