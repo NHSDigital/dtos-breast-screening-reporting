df0c4d8d984d9985da306e7ed25c598c530857d2
samw-nhs <172405776+samw-nhs@users.noreply.github.com>
2026-04-14 16:28:45 +0100

    DSTA-516: small fix

--- infrastructure/environments/dev/providers.tf
+++ infrastructure/environments/dev/providers.tf
@@ -3,7 +3,6 @@ provider "azurerm" {
 }
 
 provider "databricks" {
-  host                        = var.databricks_host
-  azure_workspace_resource_id = var.databricks_workspace_resource_id
+  host = var.databricks_host
   # Auth auto-detected: Azure CLI locally, ARM_CLIENT_SECRET in CI
 }
