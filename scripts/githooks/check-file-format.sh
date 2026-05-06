6338f6770f8c152969361dc2f46cd1b54960b728
wlyagoba <william.lyagoba1@nhs.net>
2026-05-06 16:54:07 +0100

    DSTA-536: update check-file-format for notebooks

--- scripts/githooks/check-file-format.sh
+++ scripts/githooks/check-file-format.sh
@@ -81,7 +81,9 @@ function run-editorconfig-natively() {
 
   # shellcheck disable=SC2046,SC2086
   ec \
-    --exclude '.git/' $dry_run_opt $($filter)
+    --exclude '.git/' \
+    --exclude '*.ipynb' \
+    $dry_run_opt $($filter)
 }
 
 # Run editorconfig in a Docker container.
@@ -101,7 +103,7 @@ function run-editorconfig-in-docker() {
   docker run --rm --platform linux/amd64 \
     --volume "$PWD":/check \
     "$image" \
-      sh -c "ec --exclude '.git/' $dry_run_opt \$($filter) /dev/null"
+      sh -c "ec --exclude '.git/' --exclude '*.ipynb' $dry_run_opt \$($filter) /dev/null"
 }
 
 # ==============================================================================
